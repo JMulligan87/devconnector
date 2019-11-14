1f7f814a3f9022858b97043dccdd29913dc033b1
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-11-13 22:49:04 -0500

    got a response from the body

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route Get api/users
-// @desc Test route
+// @desc Register user
 // @access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('User route');
+});
 
 module.exports = router;
