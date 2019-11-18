45a7d12ccfa4f4778fa841c083cacaeec05ad9be
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-11-17 21:06:11 -0500

    added alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,12 +12,13 @@ import store from './store';
 import './App.css';
 
 const App = () => (
-  <Provider>
+  <Provider store={store}>
     <Router>
       <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
