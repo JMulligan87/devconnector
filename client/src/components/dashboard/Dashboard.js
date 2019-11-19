f4749d3a536082f28cc18b05a7de7e330dfbde0e
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-11-18 21:17:31 -0500

    made a private route for dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,1 +1,10 @@
-import
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = props => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
