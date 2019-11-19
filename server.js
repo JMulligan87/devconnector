05312b6f7a5366997ec6c8be4babd5da47d98837
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-11-18 20:59:45 -0500

    added a logout function

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+// const path = require('path');
 
 const app = express();
 
@@ -14,8 +15,8 @@ app.get('/', (req, res) => res.send('API Running'));
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
