19db4a8a65f86f7bb330ad538ee614911c23e48b
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-11-20 16:05:08 -0500

    can now like posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,9 @@
-import { GET_POSTS, POST_ERROR } from '../actions/types';
+import {
+  GET_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -11,6 +16,12 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+        loading: false
+      };
     case GET_POSTS:
       return {
         ...state,
@@ -21,6 +32,14 @@ export default function(state = initialState, action) {
       return {
         ...state,
         error: payload,
+        loading: false
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
         loading: false
       };
     default:
